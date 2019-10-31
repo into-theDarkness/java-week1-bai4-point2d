57ef4c925c5c968f4250fa292fd618454d4f0310
into-theDarkness <zzpeterpan0510@gmail.com>
2019-10-31 22:10:43 +0700

    point2d

---
+++ src/Point2D.java
@@ -0,0 +1,29 @@
+public class Point2D {
+    public float x = 0.0f;
+    public float y = 0.0f;
+    public Point2D(){
+
+    }
+    public Point2D(float x, float y){
+        this.x = x;
+        this.y = y;
+
+    }
+    public float getX(){
+        return x;
+    }
+    public float getY(){
+        return y;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+    public float getXY(){
+        return x;
+    }
+}
